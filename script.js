7a8f91d7abe1d9f809cfa496b3dbe466c2591d01
Surendra906 <surendramode945@gmail.com>
2026-09-22 12:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+	
+let elements=document.querySelectorAll('strong');
+	elements.forEach(el=>{
+		el.style.color='rgb(0, 128, 0)';
+	});
+	
 
 }
 
 
 function return_normal() {
+	
     //Write your code here
-
-    
+		
+let elements=document.querySelectorAll('strong');
+	elements.forEach(el=>{
+		el.style.color='rgb(0, 0, 0)';
+	});    
 }
